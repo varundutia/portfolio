e8a4a7a4f48baba722ec71614931565d54c7f873
Varun Hameer Dutia <varun.dutia@outlook.com>
2025-10-13 10:24:52 +0100

    Navbar issues and blending issues

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,38 +8,40 @@ import EmailIcon from '@mui/icons-material/Email';
 const About = () => {
     return (
         <>
-            <div className="about-main">
-                <div className="about-heading">
-                    <span className='hello'>Hello 👋 </span>
-                    <span className='hero-heading'>I am <span className="name">Varun Dutia</span></span>
-                    <Typical
-                        steps={[
-                            'Senior Software Developer', 2000,
-                            'Frontend Developer', 2000,
-                            'Backend Developer', 2000,
-                            'App Developer', 2000,
-                            'Full Stack Engineer', 2000,
-                            'Cloud & DevOps Learner', 2000
-                        ]}
-                        loop={Infinity}
-                        wrapper="h2"
-                        className="hero-typical"
-                    />
-                </div>
+            <div className="about-container">
+                <div className="about-main">
+                    <div className="code-background"></div>
+                    <div className="about-heading">
+                        <span className='hello'>Hello 👋 </span>
+                        <span className='hero-heading'>I am <span className="name">Varun Dutia</span></span>
+                        <Typical
+                            steps={[
+                                'Senior Software Developer', 2000,
+                                'Frontend Developer', 2000,
+                                'Backend Developer', 2000,
+                                'App Developer', 2000,
+                                'Full Stack Engineer', 2000,
+                                'Cloud & DevOps Learner', 2000
+                            ]}
+                            loop={Infinity}
+                            wrapper="h2"
+                            className="hero-typical"
+                        />
+                    </div>
 
-                <div className="socials">
-                    <a href="https://github.com/varundutia" target="_blank" rel="noreferrer">
-                        <GitHubIcon fontSize="large" className="icon" />
-                    </a>
-                    <a href="https://www.linkedin.com/in/varun-dutia" target="_blank" rel="noreferrer">
-                        <LinkedInIcon fontSize="large" className="icon" />
-                    </a>
-                    <a href="mailto:varundutia.hameer@gmail.com" target="_blank" rel="noreferrer">
-                        <EmailIcon fontSize="large" className="icon" />
-                    </a>
+                    <div className="socials">
+                        <a href="https://github.com/varundutia" target="_blank" rel="noreferrer">
+                            <GitHubIcon fontSize="large" className="icon" />
+                        </a>
+                        <a href="https://www.linkedin.com/in/varun-dutia" target="_blank" rel="noreferrer">
+                            <LinkedInIcon fontSize="large" className="icon" />
+                        </a>
+                        <a href="mailto:varundutia.hameer@gmail.com" target="_blank" rel="noreferrer">
+                            <EmailIcon fontSize="large" className="icon" />
+                        </a>
+                    </div>
                 </div>
             </div>
-
         </>
     )
 }
